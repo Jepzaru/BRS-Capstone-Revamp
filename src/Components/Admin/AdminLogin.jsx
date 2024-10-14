ce8aa8aca0a5de086cd141080a6c62faa5c2c315
carefreebee <leonelchristiebaclayon@gmail.com>
2024-10-14 21:47:16 +0800

    fix

--- src/Components/Admin/AdminLogin.jsx
+++ src/Components/Admin/AdminLogin.jsx
@@ -35,7 +35,7 @@ const AdminLogin = () => {
     setError(null);
 
     try {
-      const response = await fetch('citumovebackend.up.railway.app/authenticate', {
+      const response = await fetch('https://citumovebackend.up.railway.app/authenticate', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
