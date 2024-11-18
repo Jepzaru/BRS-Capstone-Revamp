36e2cf983e790633eb547ceec754c45ae6fdb4b2
Jepzaru <jefffrancis.conson@cit.edu>
2024-11-18 22:19:40 +0800

    cors fixes

--- Backend/src/main/java/com/brscapstone1/brscapstone1/Configuration/WebConfig.java
+++ Backend/src/main/java/com/brscapstone1/brscapstone1/Configuration/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
 	        @Override
 	        public void addCorsMappings(CorsRegistry registry) {
 	            registry.addMapping("/**")
-	                    .allowedOrigins("http://localhost:5173")  
+						.allowedOrigins("https://citumove.vercel.app")
 	                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
 	                    .allowedHeaders("*")
 	                    .allowCredentials(true);
